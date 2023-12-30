4f934ebd7864d681c16dadfd6d9bf1376df6ce47
udaydurgam <udaydurgam26@gmail.com>
2023-12-30 14:25:48 +0530

    Added result

--- StudentResult.java
+++ StudentResult.java
@@ -2,22 +2,33 @@ import java.util.Scanner;
 
 public class StudentResult {
     public static void main(String[] args) {
+        // Create a Scanner object to take user inputs
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter marks for 5 subjects:");
+        // Take inputs for each subject
+        System.out.print("Enter marks for Maths: ");
+        int Subject1 = scanner.nextInt();
 
-        for (int i = 1; i <= 5; i++) {
-            System.out.print("Subject " + i + ": ");
-            int marks = scanner.nextInt();
+        System.out.print("Enter marks for English: ");
+        int Subject2 = scanner.nextInt();
 
-            if (marks < 35)
-                System.out.println("Failed");
-            else
-                System.out.println("Passed");
-            // You can perform further processing or store the marks as needed
-        }
+        System.out.print("Enter marks for Bio: ");
+        int Subject3 = scanner.nextInt();
 
-        // Closing the scanner
+        System.out.print("Enter marks for Science: ");
+        int Subject4 = scanner.nextInt();
+
+        System.out.print("Enter marks for Physics: ");
+        int Subject5 = scanner.nextInt();
+
+        // Close the scanner to avoid resource leak
         scanner.close();
+
+        // Check if the student passed or failed
+        if (Subject1 >= 35 && Subject2 >= 35 && Subject3 >= 35 && Subject4 >= 35 && Subject5 >= 35) {
+            System.out.println(" Passed.");
+        } else {
+            System.out.println("Failed.");
+        }
     }
 }
